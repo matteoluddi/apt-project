1f69eee6294a649f969fbe2d32ce9ed9ac6f76c4
matteo <matteo.luddi@gmail.com>
2023-07-16 16:40:03 +0200

    Changed class name, from ..test to ..steps

--- com.luddi.matteo.airport/src/bdd/java/com/luddi/matteo/airport/bdd/steps/FlightSwingViewSteps.java
+++ com.luddi.matteo.airport/src/bdd/java/com/luddi/matteo/airport/bdd/steps/FlightSwingViewSteps.java
@@ -21,7 +21,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class FlightSwingViewTest {
+public class FlightSwingViewSteps {
 
 	private FrameFixture window;
 
