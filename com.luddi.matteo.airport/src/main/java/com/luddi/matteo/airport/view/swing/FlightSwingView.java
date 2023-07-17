3ded65aefe5ad5ced8bf80162415ee58b670b698
matteo <matteo.luddi@gmail.com>
2023-07-17 10:18:19 +0200

    Changed a listener in passengersNumberTextField. Modified with a new KeyAdapter

--- com.luddi.matteo.airport/src/main/java/com/luddi/matteo/airport/view/swing/FlightSwingView.java
+++ com.luddi.matteo.airport/src/main/java/com/luddi/matteo/airport/view/swing/FlightSwingView.java
@@ -141,26 +141,14 @@ public class FlightSwingView extends JFrame implements FlightView {
 		contentPane.add(lblPassengersNumber, gbc_lblPassengersNumber);
 		
 		passengersNumberTextField = new JTextField();
-		passengersNumberTextField.addKeyListener(new KeyListener() {
-			 @Override
-	            public void keyTyped(KeyEvent e) {
-	                char c = e.getKeyChar();
-	                if (!Character.isDigit(c)) {
-	                	e.consume();
-	                }
-	            }
-
+		passengersNumberTextField.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent e) {
-				//no behavior
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				//no behavior
-			}
-
-			
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c)) {
+                	e.consume();
+                }
+            }		
 		});
 		
 		passengersNumberTextField.addKeyListener(btnAddEnabler);
